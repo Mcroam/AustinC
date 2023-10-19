32d8574f4b2768f79affdfdc3b2271fabc04375e
Mcroam <145292880+Mcroam@users.noreply.github.com>
2023-10-19 06:50:13 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-    int a, b, c;
-    printf("Enter the first value:");
-    scanf("%d", &a);
-    printf("Enter the second value:");
-    scanf("%d", &b);
-    c = a + b;
-    printf("%d + %d = %d\n", a, b, c);
+    float a, b;
+    a = 0;
+    b = -1;
+    while (a <=200){
+        if ((a > 98.6) && (b < 98.6)){
+            printf("%6.2f degrees F = %6.2f degrees C\n",
+                98.6, (98.6 - 32.0) * 5.0 / 9.0);
+        }
+        printf("%6.2f degrees F =%6.2f degrees C\n",
+            a, (a - 32.0) * 5.0 / 9.0);
+        b = a;
+        a = a + 10;
+    }
     return 0;
 }
